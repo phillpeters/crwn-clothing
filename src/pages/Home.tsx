82e4163a512f612f2b456ad612b6cc2ef06c6147
Phill Peters <phillpeters1@gmail.com>
2023-03-29 17:05:11 -0600

    install sass, create and style initial categories layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,42 @@
+import '../Categories.scss';
+
 const Home = () => {
-  return <h1>Hello World</h1>;
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    }
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div className="category-container" key={id}>
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
